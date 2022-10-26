54c6ad6a388066411cfde536c01964cd6d1d23eb
Yokota <ryok90@gmail.com>
2022-10-26 12:12:57 -0300

    fix: theme dark

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css'
+import theme from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
