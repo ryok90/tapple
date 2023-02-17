6837da0585587935511360a4ba86b4fddfdea9a9
Yokota <ryok90@gmail.com>
2023-02-16 21:53:57 -0300

    fix: manifest icons

--- vite.config.ts
+++ vite.config.ts
@@ -16,17 +16,17 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/tapple/android-chrome-192x192.png',
+            src: 'android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/tapple/android-chrome-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/tapple/android-chrome-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
